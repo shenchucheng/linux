9439a08f4a01b53b6a812fe6998c71da40b0aece
shenchucheng <chuchengshen@fuzhi.ai>
2020-10-20 16:16:57 +0800

    modify the leanote

--- leanote/backup_leanote.sh
+++ leanote/backup_leanote.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/sh
 # leanote 文件及数据库备份
 # USAGE:
-##       backup_leanote password passwd  address >> /var/log/backup_leanote.log 2>&1
+##       backup_leanote password prefix  address >> /var/log/backup_leanote.log 2>&1
 ##       openssl enc -d -aes256 -in *.tar.gz | tar xz
 
 FILENAME=leanote_mongodb_backup_`date +%Y_%m_%d`
-BACKUPDIR=/tmp/leanote/
+BACKUPDIR=$HOME/.temp/
 LEANOTEDIR=/home/cheng/backup/leanote
-mongodump -d leanote -u leanote -p $1 -o $BACKUPDIR$FILENAME
+# mongodump -d leanote -u leanote -p $1 -o $BACKUPDIR$FILENAME
+mongodump -d leanote -o $BACKUPDIR$FILENAME
 tar -czf - -C $BACKUPDIR $FILENAME | openssl enc -e -aes256 -out $LEANOTEDIR"/mongodb_backup/"$FILENAME".tar.gz" -k $1
-rsync -L -arvztpog --delete --progress $LEANOTEDIR cheng@$3:/home/cheng/backup
+rsync -L -arvztpog --delete --progress $LEANOTEDIR cheng@$3:/home/cheng/backup/$2
 rm -rvf $BACKUPDIR$FILENAME 
